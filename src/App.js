9c94050191b44d314df390c117eb63d73ae9e51d
khankingtauseef <37797944+khankingtauseef@users.noreply.github.com>
2019-10-08 08:09:13 +0530

    Fresh react app
    download and run npn install in a command terminal,

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div>
+      <span>Hello World! {props.name}</span>      
+    </div>
+  );
+}
+
+export default App;
